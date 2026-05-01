cd4b3e34c53940c9de5a3c52918025f2d31def1b
thesourorange <thesourorange@outlook.com>
2026-05-01 19:41:05 +1000

    Fixed up formatting

--- assets/scripts/Bullet.js
+++ assets/scripts/Bullet.js
@@ -1,17 +1,17 @@
 class Bullet {
-  constructor(x,y, level) {
-    this.x = x-3;
-    this.y = y+6;
-    this.height = 12;
-    this.width = 3;
-    this.level = level;
-  }
+    constructor(x, y, level) {
+        this.x = x - 3;
+        this.y = y + 6;
+        this.height = 12;
+        this.width = 3;
+        this.level = level;
+    }
 
-  draw(context) {
-    var bulletColorArray = ['#ed3323', '#71f98d', '#fffd54', '#0027f5', '#eb41f7', '#f18533'];
-    context.fillStyle = bulletColorArray[this.level];
-    context.fillRect(this.x,this.y,this.width,this.height);
-  }
+    draw(context) {
+        var bulletColorArray = ['#ed3323', '#71f98d', '#fffd54', '#0027f5', '#eb41f7', '#f18533'];
+        context.fillStyle = bulletColorArray[this.level];
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
 
 }
 
--- assets/scripts/Centipede-Whole.js
+++ assets/scripts/Centipede-Whole.js
@@ -1,83 +1,83 @@
 const CentipedeSeg = require('./Centipede-Segment.js');
 
 class CentipedeWhole {
-  constructor(level) {
-    this.segmentsArray = [[]];
-    this.y = 0;
-    this.segmentFrames = 0;
-    this.yOffset = 24;
-    this.yMin = 0;
-    this.level = level;
-  }
-
-  createCentipede() {
-    for (var i = 0; i < 13; i++) {
-      this.segmentsArray[0].push(new CentipedeSeg(i*24,21,24));
+    constructor(level) {
+        this.segmentsArray = [[]];
+        this.y = 0;
+        this.segmentFrames = 0;
+        this.yOffset = 24;
+        this.yMin = 0;
+        this.level = level;
     }
-  }
 
-  draw (context) {
-    this.segmentsArray.forEach((centipede) => {
-      centipede.forEach((segment, index, segments) => {
-        var centipedeImg = document.getElementById('centipede');
-        if (index === segments.length - 1) {
-          segment.isHead = true;
-        } else {
-          segment.isHead = false;
+    createCentipede() {
+        for (var i = 0; i < 13; i++) {
+            this.segmentsArray[0].push(new CentipedeSeg(i * 24, 21, 24));
         }
-        segment.headAnimation();
-        if (segment.speed < 0 ){
-          context.drawImage(centipedeImg, segment.animationState * 84 + 840, this.level*96, 84, 96, segment.x, segment.y, segment.h, segment.w);
-        } else {
-          context.drawImage(centipedeImg, segment.animationState * 84, this.level*96, 84, 96, segment.x, segment.y, segment.h, segment.w);
-        }
-      });
-    });
-  }
+    }
 
-  move (mushrooms) {
-    this.segmentsArray.forEach((centipede) => { 
-      centipede.forEach((segment) => { 
-        mushrooms.forEach((mushroom) => {
-          if (segment.speed > 0 && (segment.x === 720-segment.speed || (segment.x + 24 === mushroom.x && segment.y === mushroom.y))) {
-            segment.y += segment.yOffset;
-            var changeDir = (-1);
-            mushrooms.forEach((shroomTwo) => {
-              if (shroomTwo.x === (segment.x - 24) && shroomTwo.y === segment.y){
-                changeDir *= -1;
-              } 
+    draw(context) {
+        this.segmentsArray.forEach((centipede) => {
+            centipede.forEach((segment, index, segments) => {
+                var centipedeImg = document.getElementById('centipede');
+                if (index === segments.length - 1) {
+                    segment.isHead = true;
+                } else {
+                    segment.isHead = false;
+                }
+                segment.headAnimation();
+                if (segment.speed < 0) {
+                    context.drawImage(centipedeImg, segment.animationState * 84 + 840, this.level * 96, 84, 96, segment.x, segment.y, segment.h, segment.w);
+                } else {
+                    context.drawImage(centipedeImg, segment.animationState * 84, this.level * 96, 84, 96, segment.x, segment.y, segment.h, segment.w);
+                }
             });
-            segment.speed *= changeDir;
-          } else if (segment.speed < 0 && (segment.x < 0 || (segment.x - 24 === mushroom.x && segment.y === mushroom.y))) {
-            var changeDir2 = (-1);
-            segment.y += segment.yOffset;
-            mushrooms.forEach((shroomThree) => {
-              if (shroomThree.x === (segment.x + 24) && shroomThree.y === segment.y){
-                changeDir2 *= -1;
-              }
-            });
-            segment.speed *= changeDir2;
-          }
         });
-        
-        segment.x += segment.speed;
-        if (segment.y === 696 && (segment.x === 0 || segment.x === 696)){
-          segment.yOffset = -24;
-          this.yMin = 552;
-        } else if (segment.y === this.yMin){
-          segment.yOffset = 24;
-        }
-      });
-    });
-  }
+    }
 
-  checkCentipedeHealth () {
-    this.segmentsArray.forEach((worm,wormIndex) => {
-      if (worm.length === 0) {
-        this.segmentsArray.splice(wormIndex,1);
-      }
-    });
-  }
+    move(mushrooms) {
+        this.segmentsArray.forEach((centipede) => {
+            centipede.forEach((segment) => {
+                mushrooms.forEach((mushroom) => {
+                    if (segment.speed > 0 && (segment.x === 720 - segment.speed || (segment.x + 24 === mushroom.x && segment.y === mushroom.y))) {
+                        segment.y += segment.yOffset;
+                        var changeDir = (-1);
+                        mushrooms.forEach((shroomTwo) => {
+                            if (shroomTwo.x === (segment.x - 24) && shroomTwo.y === segment.y) {
+                                changeDir *= -1;
+                            }
+                        });
+                        segment.speed *= changeDir;
+                    } else if (segment.speed < 0 && (segment.x < 0 || (segment.x - 24 === mushroom.x && segment.y === mushroom.y))) {
+                        var changeDir2 = (-1);
+                        segment.y += segment.yOffset;
+                        mushrooms.forEach((shroomThree) => {
+                            if (shroomThree.x === (segment.x + 24) && shroomThree.y === segment.y) {
+                                changeDir2 *= -1;
+                            }
+                        });
+                        segment.speed *= changeDir2;
+                    }
+                });
+
+                segment.x += segment.speed;
+                if (segment.y === 696 && (segment.x === 0 || segment.x === 696)) {
+                    segment.yOffset = -24;
+                    this.yMin = 552;
+                } else if (segment.y === this.yMin) {
+                    segment.yOffset = 24;
+                }
+            });
+        });
+    }
+
+    checkCentipedeHealth() {
+        this.segmentsArray.forEach((worm, wormIndex) => {
+            if (worm.length === 0) {
+                this.segmentsArray.splice(wormIndex, 1);
+            }
+        });
+    }
 }
 
 // var centipedeMoveSound = new Audio('assets/sounds/centipede-move.wav');
